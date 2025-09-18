0f4e4934e083a2103b82a43ab56d7e17cb2dc13a
northerntreegit <yuting.hit@gmail.com>
2025-09-18 14:05:02 +0800

    test func

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 //% color=#EC1313 weight=91 icon="\uf19c" block="LU-Bit"
 namespace luBit {
-    //% block="Init RC-Controller"
+    export enum DroneOrder {
+        //% block="起飞"
+        TakeOff = 1,
+        //% block="降落"
+        Land = 2,
+        //% block="紧急停机"
+        EmergencyStop = 3,
+        //% block="校准"
+        Calibrate = 4,
+        //% block="打开悬停"
+        HoverOn = 5,
+        //% block="关闭悬停"
+        HoverOff = 6
+    }
+
+    //% block="Init Drone RC-Controller"
     //% weight=100
     //% group="LU_Drone"
     export function RC_Controller_Init(): void {
@@ -11,9 +26,7 @@ namespace luBit {
         )
     }
 
-
-
-    //% block="Control throttle=$throttle pitch=$pitch roll=$roll yaw=$yaw"
+    //% block="Set throttle=$throttle pitch=$pitch roll=$roll yaw=$yaw"
     //% weight=90
     //% group="LU_Drone"
     export function sendRC_Control(throttle: number, pitch: number, roll: number, yaw: number): void {
@@ -22,11 +35,12 @@ namespace luBit {
 
 
 
-    //% block="Order order=$order data=$data"
+    //% block="Send order=$order"
     //% weight=80
     //% group="LU_Drone"
-    export function sendRC_Order(order: number, data: number): void {
-        serial.writeNumber(1)
+    //% order.defl=DroneOrder.TakeOff
+    export function sendRC_Order(order: DroneOrder): void {
+        serial.writeNumber(order)
     }
 
 }
